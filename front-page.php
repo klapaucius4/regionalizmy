92d73ecce6223ba05db912518ef4d9334e113a3d
klapaucius4 <klapaucius.mk@gmail.com>
2020-03-10 17:09:43 +0100

    modifications

--- front-page.php
+++ front-page.php
@@ -28,7 +28,7 @@
   <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand" href="<?= home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/img/logo.png" alt="Logo" /></a>
+      <a class="navbar-brand p-0" href="<?= home_url(); ?>"><img style="width: 300px;" src="<?= get_template_directory_uri(); ?>/img/logo.png" alt="Logo" /></a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
