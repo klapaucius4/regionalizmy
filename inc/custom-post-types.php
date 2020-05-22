a9cd38e0eb218ab8da1bac9832eb96d8cbccbbad
klapaucius4 <klapaucius.mk@gmail.com>
2020-05-22 17:27:48 +0200

    mods

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -21,5 +21,26 @@ register_post_type('regionalizmy_county', array(
         'public' => true,
         'supports' => array( 'title' ),
         'menu_icon' => 'dashicons-admin-site',
+        'taxonomies' => array('regionalizmy_province'),
     )
 );
+register_taxonomy('regionalizmy_province', array('regionalizmy_county'), array(
+    'labels' => array(
+        'name' => __('Województwa', 'regionalizmy'),
+        'singular_name' => __('Województwo', 'regionalizmy'),
+        'search_items' =>  __( 'Szukaj województwa' ),
+        'all_items' => __( 'Wszystkie województwa' ),
+    //     'parent_item' => __( 'Parent Type' ),
+    //     'parent_item_colon' => __( 'Parent Type:' ),
+    //     'edit_item' => __( 'Edit Type' ), 
+    //     'update_item' => __( 'Update Type' ),
+    //     'add_new_item' => __( 'Add New Type' ),
+    //     'new_item_name' => __( 'New Type Name' ),
+    //     'menu_name' => __( 'Types' ),
+    ),
+    'hierarchical' => true,
+    'show_ui' => true,
+    'show_admin_column' => true,
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'type' ),
+  ));
