c4ad9c83c1610347fb88319f4a365cac63212925
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-25 23:38:18 +0200

    mods

--- front-page.php
+++ front-page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
-<!-- <section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <div id="map" style="width: 100%; height: 100vh;"></div>
+  <section>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div id="map" style="width: 100%; height: 100vh;"></div>
+        </div>
       </div>
     </div>
-  </div>
-</section> -->
+  </section>
 
   <!-- Main Content -->
   <div class="container">
