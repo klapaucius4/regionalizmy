158491e0785d134bef60fe149c476ea761163e20
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-26 23:37:43 +0200

    mods

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
   </nav>
 
   <!-- Page Header -->
-  <header class="masthead">
+  <header class="masthead" style="background-image: url('<?= get_template_directory_uri(); ?>/img/front-page-bg.svg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
