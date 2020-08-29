62913f0734107a474339ccdd1977176de013230c
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-29 19:31:47 +0200

    mods

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="pl">
+<html <?php language_attributes(); ?>>
 
 <head>
 
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
+    <!-- <meta name="description" content=""> -->
     <meta name="author" content="">
 
     <title>Clean Blog - Start Bootstrap Theme</title>
