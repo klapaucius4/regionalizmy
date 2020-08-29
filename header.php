bcc096db66d16d9aa0b77f0d22941a78abdfde5e
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-29 19:12:20 +0200

    mods

--- header.php
+++ header.php
@@ -58,7 +58,8 @@
 
   <!-- Page Header -->
   <header class="masthead">
-    <div class="overlay"></div>
+    <div class="overlay__1"></div>
+    <div class="overlay__2"></div>
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
