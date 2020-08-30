7e908048102d324899e0da4a78ca673729955788
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-30 22:54:27 +0200

    mods

--- front-page.php
+++ front-page.php
@@ -1,18 +1,19 @@
 <?php get_header(); ?>
 
-  <!-- Main Content -->
-  <?php while(have_posts()): the_post(); ?>
+<section>
   <div class="container">
     <div class="row">
       <div class="col-md-7">
-        <?php the_content(); ?>
+        <?php while(have_posts()): the_post(); ?>
+          <?php the_content(); ?>
+        <?php endwhile; wp_reset_postdata(); ?>
       </div>
       <div class="col-md-5">
         <img src="<?= get_template_directory_uri(); ?>/img/front-desc.svg" class="w-100" alt="<?= bloginfo('name'); ?>" />
       </div>
     </div>
   </div>
-  <?php endwhile; wp_reset_postdata(); ?>
+  </section>
 
   <hr>
 
@@ -26,46 +27,49 @@
     </div>
   </section>
 
-  <!-- Main Content -->
-  <div class="container">
-    <div class="row">
-    <?php
-    $args = array(
-      'post_type' => 'regionalizmy_phrase',
-      'posts_per_page' => -1
-    );
-    $myQuery = new WP_Query($args);
-    if($myQuery->have_posts()):
-    ?>
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <?php while($myQuery->have_posts()): $myQuery->the_post(); ?>
-          <div class="post-preview">
-            <a href="<?= get_the_permalink(); ?>">
-              <h2 class="post-title">
-                <?= get_the_title(); ?>
-              </h2>
-              <h3 class="post-subtitle">
-                Problems look mighty small from 150 miles up
-              </h3>
-            </a>
-            <p class="post-meta"><?= __('Dodane przez'); ?> <a href="#"><?= get_the_author(); ?></a> <?= get_the_date(); ?></p>
-            <p>
-              <button class="btn btn-success" onclick="toggleOn()">Znam</button>
-              <button class="btn btn-danger" onclick="toggleOff()">Nie znam</button>
-            </p>
+
+  <section>
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+      <?php
+      $args = array(
+        'post_type' => 'regionalizmy_phrase',
+        'posts_per_page' => -1
+      );
+      $myQuery = new WP_Query($args);
+      if($myQuery->have_posts()):
+      ?>
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <?php while($myQuery->have_posts()): $myQuery->the_post(); ?>
+            <div class="post-preview">
+              <a href="<?= get_the_permalink(); ?>">
+                <h2 class="post-title">
+                  <?= get_the_title(); ?>
+                </h2>
+                <h3 class="post-subtitle">
+                  Problems look mighty small from 150 miles up
+                </h3>
+              </a>
+              <p class="post-meta"><?= __('Dodane przez'); ?> <a href="#"><?= get_the_author(); ?></a> <?= get_the_date(); ?></p>
+              <p>
+                <button class="btn btn-success" onclick="toggleOn()">Znam</button>
+                <button class="btn btn-danger" onclick="toggleOff()">Nie znam</button>
+              </p>
+            </div>
+            <hr>
+          <?php endwhile; ?>
+          <!-- Pager -->
+          <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
           </div>
-          <hr>
-        <?php endwhile; ?>
-        <!-- Pager -->
-        <div class="clearfix">
-          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
         </div>
+      <?php
+      endif;
+      ?>
       </div>
-    <?php
-    endif;
-    ?>
     </div>
-  </div>
+  </section>
 
 <?php
 get_footer();
