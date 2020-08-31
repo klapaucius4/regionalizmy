752953077a7caf73c394aa81ea31446b97861a13
klapaucius4 <klapaucius.mk@gmail.com>
2020-08-31 19:12:58 +0200

    mods

--- inc/helpers.php
+++ inc/helpers.php
@@ -27,8 +27,8 @@ function bootstrap_pagination( \WP_Query $wp_query = null, $echo = true, $params
             'end_size'     => 3,
             'mid_size'     => 1,
             'prev_next'    => true,
-            'prev_text'    => __( '« Prev' ),
-            'next_text'    => __( 'Next »' ),
+            'prev_text'    => __( '« Wstecz' ),
+            'next_text'    => __( 'Dalej »' ),
             'add_args'     => $add_args,
             'add_fragment' => ''
         ], $params )
