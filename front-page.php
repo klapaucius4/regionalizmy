973e51b8967a20da78006e8f5e0f9305cf75c9bd
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-01 00:17:33 +0200

    mods

--- front-page.php
+++ front-page.php
@@ -63,7 +63,7 @@
                 <p class="post-meta"><?= __('Dodane przez'); ?> <a href="#"><?= get_the_author(); ?></a> <?= get_the_date(); ?></p>
               </div>
               <div class="col-md-4 d-flex justify-content-end align-items-center">
-                <button class="btn btn-success"><?= __('Znam'); ?></button>
+                <button class="btn btn-success mr-1"><?= __('Znam'); ?></button>
                 <button class="btn btn-danger"><?= __('Nie znam'); ?></button>
               </div>
             </div>
