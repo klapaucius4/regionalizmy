ddab969dd75bc256396333ba49a814dfc55153fc
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-01 21:02:37 +0200

    mods

--- page-faq.php
+++ page-faq.php
@@ -10,18 +10,18 @@ get_header();
           <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
           <?php
-          $faqElements = get_field('faq_elements');
-          if($faqElements):
-            foreach($faqElements as $el): ?>
+          $faqList = get_field('lista_faq');
+          if($faqList):
+            foreach($faqList as $el): ?>
               <div class="panel panel-default">
                 <div class="panel-heading p-3 mb-3" role="tab" id="heading<?= $i; ?>">
                 <h3 class="panel-title">
-                  <a class="arrow collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse<?= $i; ?>" aria-expanded="true" aria-controls="collapse<?= $i; ?>"><?= $el['title']; ?></a>
+                  <a class="arrow collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse<?= $i; ?>" aria-expanded="true" aria-controls="collapse<?= $i; ?>"><?= $el['tytul']; ?></a>
                 </h3>
                 </div>
                 <div id="collapse<?= $i; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?= $i; ?>">
                 <div class="panel-body px-3 mb-4">
-                  <p class="m-0"><?= nl2br($el['content']); ?></p>
+                  <p class="m-0"><?= nl2br($el['tresc']); ?></p>
                 </div>
                 </div>
               </div>
