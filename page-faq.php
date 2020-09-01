3128b80a5e0e21eba1b2c7dc30f0d1918c911e4b
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-01 20:52:52 +0200

    mods

--- page-faq.php
+++ page-faq.php
@@ -9,67 +9,24 @@ get_header();
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
-          <?php for($i=0; $i<=20; $i++): ?>
-            <div class="panel panel-default">
-              <div class="panel-heading p-3 mb-3" role="tab" id="heading<?= $i; ?>">
-              <h3 class="panel-title">
-                <a class="arrow collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse<?= $i; ?>" aria-expanded="true" aria-controls="collapse<?= $i; ?>">
-                What are the benefits of Solodev CMS?
-                </a>
-              </h3>
+          <?php
+          $faqElements = get_field('faq_elements');
+          if($faqElements):
+            foreach($faqElements as $el): ?>
+              <div class="panel panel-default">
+                <div class="panel-heading p-3 mb-3" role="tab" id="heading<?= $i; ?>">
+                <h3 class="panel-title">
+                  <a class="arrow collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse<?= $i; ?>" aria-expanded="true" aria-controls="collapse<?= $i; ?>"><?= $el['title']; ?></a>
+                </h3>
+                </div>
+                <div id="collapse<?= $i; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?= $i; ?>">
+                <div class="panel-body px-3 mb-4">
+                  <p class="m-0"><?= nl2br($el['content']); ?></p>
+                </div>
+                </div>
               </div>
-              <div id="collapse<?= $i; ?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?= $i; ?>">
-              <div class="panel-body px-3 mb-4">
-                <p class="">With Solodev CMS, you and your visitors will benefit from a finely-tuned technology stack that drives the highest levels of site performance, speed and engagement - and contributes more to your bottom line. Our users fell in love with:</p>
-              </div>
-              </div>
-            </div>
-          <?php endfor; ?>
-            
-            <!-- <div class="panel panel-default">
-              <div class="panel-heading p-3 mb-3" role="tab" id="heading1">
-              <h3 class="panel-title">
-                <a class="collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                How easy is it to build a website with Solodev CMS?
-                </a>
-              </h3>
-              </div>
-              <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
-              <div class="panel-body px-3 mb-4">
-                <p>Building a website is extremely easy. With a working knowledge of HTML and CSS you will be able to have a site up and running in no time.</p>
-              </div>
-              </div>
-            </div>
-            
-            <div class="panel panel-default">
-              <div class="panel-heading p-3 mb-3" role="tab" id="heading2">
-              <h3 class="panel-title">
-                <a class="collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="true" aria-controls="collapse2">
-                What is the uptime for Solodev CMS?
-                </a>
-              </h3>
-              </div>
-              <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2">
-              <div class="panel-body px-3 mb-4">
-                <p>Using Amazon AWS technology which is an industry leader for reliability you will be able to experience an uptime in the vicinity of 99.95%.</p>
-              </div>
-              </div>
-            </div>
-            
-            <div class="panel panel-default">
-              <div class="panel-heading p-3 mb-3" role="tab" id="heading3">
-              <h3 class="panel-title">
-                <a class="collapsed" role="button" title="" data-toggle="collapse" data-parent="#accordion" href="#collapse3" aria-expanded="true" aria-controls="collapse3">
-                Can Solodev CMS handle multiple websites for my company?
-                </a>
-              </h3>
-              </div>
-              <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3">
-              <div class="panel-body px-3 mb-4">
-                <p>Yes, Solodev CMS is built to handle the needs of any size company. With our Multi-Site Management, you will be able to easily manage all of your websites.</p>
-              </div>
-              </div>
-            </div> -->
+            <?php endforeach;
+          endif; ?>
           </div>
         </div>
       </div>
