01c03d6139e6abe353b524344b0e105294129327
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-04 00:54:41 +0200

    dev

--- inc/rgm-rest-routes.php
+++ inc/rgm-rest-routes.php
@@ -6,57 +6,87 @@ class RGM_REST_Routes extends WP_REST_Controller {
    * Register the routes for the objects of the controller.
    */
   public function register_routes() {
-    $version = '1';
-    $namespace = 'vendor/v' . $version;
+    // $version = '1';
+    // $namespace = 'vendor/v' . $version;
+    $namespace = 'rgm';
     $base = 'route';
-    register_rest_route( $namespace, '/' . $base, array(
-      array(
-        'methods'             => WP_REST_Server::READABLE,
-        'callback'            => array( $this, 'get_items' ),
-        'permission_callback' => array( $this, 'get_items_permissions_check' ),
-        'args'                => array(
+    // register_rest_route( $namespace, '/' . $base, array(
+    //   array(
+    //     'methods'             => WP_REST_Server::READABLE,
+    //     'callback'            => array( $this, 'get_items' ),
+    //     'permission_callback' => array( $this, 'get_items_permissions_check' ),
+    //     'args'                => array(
  
-        ),
-      ),
-      array(
-        'methods'             => WP_REST_Server::CREATABLE,
-        'callback'            => array( $this, 'create_item' ),
-        'permission_callback' => array( $this, 'create_item_permissions_check' ),
-        'args'                => $this->get_endpoint_args_for_item_schema( true ),
-      ),
-    ) );
-    register_rest_route( $namespace, '/' . $base . '/(?P<id>[\d]+)', array(
-      array(
-        'methods'             => WP_REST_Server::READABLE,
-        'callback'            => array( $this, 'get_item' ),
-        'permission_callback' => array( $this, 'get_item_permissions_check' ),
-        'args'                => array(
-          'context' => array(
-            'default' => 'view',
-          ),
-        ),
-      ),
-      array(
-        'methods'             => WP_REST_Server::EDITABLE,
-        'callback'            => array( $this, 'update_item' ),
-        'permission_callback' => array( $this, 'update_item_permissions_check' ),
-        'args'                => $this->get_endpoint_args_for_item_schema( false ),
-      ),
-      array(
-        'methods'             => WP_REST_Server::DELETABLE,
-        'callback'            => array( $this, 'delete_item' ),
-        'permission_callback' => array( $this, 'delete_item_permissions_check' ),
-        'args'                => array(
-          'force' => array(
-            'default' => false,
-          ),
-        ),
-      ),
-    ) );
-    register_rest_route( $namespace, '/' . $base . '/schema', array(
+    //     ),
+    //   ),
+    //   array(
+    //     'methods'             => WP_REST_Server::CREATABLE,
+    //     'callback'            => array( $this, 'create_item' ),
+    //     'permission_callback' => array( $this, 'create_item_permissions_check' ),
+    //     'args'                => $this->get_endpoint_args_for_item_schema( true ),
+    //   ),
+    // ) );
+    // register_rest_route( $namespace, '/' . $base . '/(?P<id>[\d]+)', array(
+    //   array(
+    //     'methods'             => WP_REST_Server::READABLE,
+    //     'callback'            => array( $this, 'get_item' ),
+    //     'permission_callback' => array( $this, 'get_item_permissions_check' ),
+    //     'args'                => array(
+    //       'context' => array(
+    //         'default' => 'view',
+    //       ),
+    //     ),
+    //   ),
+    //   array(
+    //     'methods'             => WP_REST_Server::EDITABLE,
+    //     'callback'            => array( $this, 'update_item' ),
+    //     'permission_callback' => array( $this, 'update_item_permissions_check' ),
+    //     'args'                => $this->get_endpoint_args_for_item_schema( false ),
+    //   ),
+    //   array(
+    //     'methods'             => WP_REST_Server::DELETABLE,
+    //     'callback'            => array( $this, 'delete_item' ),
+    //     'permission_callback' => array( $this, 'delete_item_permissions_check' ),
+    //     'args'                => array(
+    //       'force' => array(
+    //         'default' => false,
+    //       ),
+    //     ),
+    //   ),
+    // ) );
+    // register_rest_route( $namespace, '/' . $base . '/schema', array(
+    //   'methods'  => WP_REST_Server::READABLE,
+    //   'callback' => array( $this, 'get_public_item_schema' ),
+    // ) );
+    register_rest_route( $namespace, '/' . $base . '/get-counties', array(
       'methods'  => WP_REST_Server::READABLE,
-      'callback' => array( $this, 'get_public_item_schema' ),
+      'callback' => array( $this, 'getCounties' ),
     ) );
+  }
+
+  public function getCounties($request){
+    $data = array();
+
+    $args = array(
+      'post_type' => 'regionalizmy_phrase',
+      'post_status' => 'publish',
+      
+    );
+
+    $myQuery = new WP_Query($args);
+
+    if($myQuery->have_posts()){
+      while($myQuery->have_posts){
+        $myQuery->the_post();
+        $data[] = array(
+          'id' => get_the_ID(),
+          'name' => get_the_title()
+        );
+      }
+      wp_reset_postdata();
+    }
+
+    return new WP_REST_Response( $data, 200 );
   }
  
   /**
