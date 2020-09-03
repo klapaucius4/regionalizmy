3a0aab443f6711e08470eca39e72a3c30495e6ff
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-03 18:54:05 +0200

    dev

--- front-page.php
+++ front-page.php
@@ -31,6 +31,64 @@
             </button>
             <label for="form-autocomplete" class="active">What is your favorite US state?</label>
           </div>
+          <script>
+          var states = [
+"Alabama",
+"Alaska",
+"Arizona",
+"Arkansas",
+"California",
+"Colorado",
+"Connecticut",
+"Delaware",
+"Florida",
+"Georgia",
+"Hawaii",
+"Idaho",
+"Illnois",
+"Indiana",
+"Iowa",
+"Kansas",
+"Kentucky",
+"Louisiana",
+"Maine",
+"Maryland",
+"Massachusetts",
+"Michigan",
+"Minnesota",
+"Mississippi",
+"Missouri",
+"Montana",
+"Nebraska",
+"Nevada",
+"New Hampshire",
+"New Jersey",
+"New Mexico",
+"New York",
+"North Carolina",
+"North Dakota",
+"Ohio",
+"Oklahoma",
+"Oregon",
+"Pennsylvania",
+"Rhode Island",
+"South Carolina",
+"South Dakota",
+"Tennessee",
+"Texas",
+"Utah",
+"Vermont",
+"Virginia",
+"Washington",
+"West Virginia",
+"Wisconsin",
+"Wyoming"
+];
+
+$('#form-autocomplete').mdbAutocomplete({
+data: states
+});
+          </script>
         </div>
         <div class="col-md-12">
           <div id="rgm-map"></div>
