63deaebb9cfdecc7a38ab281ae9d2da71e9306d1
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-03 21:44:51 +0200

    dev

--- front-page.php
+++ front-page.php
@@ -21,7 +21,7 @@
         <div class="col-md-12 mb-5 text-center">
           <h4>Skąd jesteś?</h4>
           <p class="m-0"><small>Zaznacz na poniższej mapie powiat z którego pochodzisz lub w którym mieszkasz.</small></p>
-          <div class="md-form">
+          <div class="col-md-6">
             <input type="search" id="form-autocomplete" class="form-control mdb-autocomplete" placeholder="dsdsdd">
             <button class="btn btn-primary" type="submit">Wybierz</button>
           </div>
