7e69d093ff9be45e6e53e0a1c8c908dc50946edd
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-04 23:08:35 +0200

    mods

--- header.php
+++ header.php
@@ -40,13 +40,11 @@
     <!-- Leaflet -->
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/vendor/leaflet/leaflet.css" />
 
-    
-
-    <!-- Custom styles for this template -->
-    <link href="<?= get_template_directory_uri(); ?>/css/clean-blog.min.css" rel="stylesheet">
-
     <!-- jquery-autocomplete -->
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/vendor/jquery-autocomplete/jquery.autocomplete.css" />
+
+    <!-- Custom styles for this template -->
+    <!-- <link href="<?= get_template_directory_uri(); ?>/css/clean-blog.min.css" rel="stylesheet"> -->
 
     <?php wp_head(); ?>
 </head>
