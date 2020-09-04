27dfcf4791a1919670df9a2c14048ab38da5eda7
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-04 23:22:28 +0200

    mods

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -39,28 +39,7 @@
   }
 
   var availableTags = [
-    "ActionScript",
-    "AppleScript",
-    "Asp",
-    "BASIC",
-    "C",
-    "C++",
-    "Clojure",
-    "COBOL",
-    "ColdFusion",
-    "Erlang",
-    "Fortran",
-    "Groovy",
-    "Haskell",
-    "Java",
-    "JavaScript",
-    "Lisp",
-    "Perl",
-    "PHP",
-    "Python",
-    "Ruby",
-    "Scala",
-    "Scheme"
+    'd'
   ];
   $( "#findCountyInput" ).autocomplete({
     source: availableTags
