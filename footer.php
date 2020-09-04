2ff193c4c7efa840b0915c94b339450a39c0a2c3
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-04 23:29:29 +0200

    mods

--- footer.php
+++ footer.php
@@ -44,7 +44,7 @@
 <!-- <script src="<?= get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 
 <!-- jquery.autocomplete -->
-<script src="<?= get_template_directory_uri(); ?>/vendor/jquery-autocomplete/jquery.autocomplete.js"></script>
+<!-- <script src="<?= get_template_directory_uri(); ?>/vendor/jquery-autocomplete/jquery.autocomplete.js"></script> -->
 
 <?php get_template_part('template-parts/map', 'logic'); ?>
 
