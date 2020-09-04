4f3fb7f406925fa6fda4a166d5f0876f3b83e86c
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-04 20:22:54 +0200

    mods

--- front-page.php
+++ front-page.php
@@ -115,7 +115,7 @@
             /*for each item in the array...*/
             for (i = 0; i < arr.length; i++) {
               /*check if the item starts with the same letters as the text field value:*/
-              // if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+              if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
                 /*create a DIV element for each matching element:*/
                 b = document.createElement("DIV");
                 /*make the matching letters bold:*/
@@ -132,7 +132,7 @@
                     closeAllLists();
                 });
                 a.appendChild(b);
-              // }
+              }
             }
         });
         /*execute a function presses a key on the keyboard:*/
