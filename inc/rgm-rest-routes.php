04529caab44a33e49e204317315de4a1d8d72f12
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-05 23:48:50 +0200

    mods

--- inc/rgm-rest-routes.php
+++ inc/rgm-rest-routes.php
@@ -71,8 +71,8 @@ class RGM_REST_Routes extends WP_REST_Controller {
       'post_type' => 'regionalizmy_county',
       'post_status' => 'publish',
       'posts_per_page' => 8,
-      'orderby' => 'relevance',
-      // 'order' => 'DESC'
+      'orderby' => 'title',
+      'order' => 'ASC'
     );
 
     if(isset($request['search'])){
