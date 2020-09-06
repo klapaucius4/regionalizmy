36b32dd9d88f121536e117f1e2f3c70bb000fc31
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-07 00:07:35 +0200

    mods

--- footer.php
+++ footer.php
@@ -41,6 +41,7 @@
 <script src="<?= get_template_directory_uri(); ?>/vendor/jquery/jquery.min.js"></script>
 <script src="<?= get_template_directory_uri(); ?>/vendor/jquery-ui/jquery-ui.min.js"></script>
 <script src="<?= get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?= get_template_directory_uri(); ?>/vendor/jquery.cookie/jquery.cookie.js"></script>
 
 
 <?php get_template_part('template-parts/map', 'logic'); ?>
