055fce44c85de1b7eae2d5b561a3f28905dc000a
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-07 16:15:41 +0200

    mods

--- template-parts/map-logic.php
+++ template-parts/map-logic.php
@@ -99,6 +99,8 @@ endwhile; wp_reset_postdata();
 
 console.log(statesData);
 
+/*
+
 var map = L.map('rgm-map').setView([51.759445, 19.457216], 7);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
