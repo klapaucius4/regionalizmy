c67999dd4a3ab045b9510fa617eb4d5560fc0f51
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-08 13:28:21 +0200

    maps

--- template-parts/map-logic.php
+++ template-parts/map-logic.php
@@ -120,6 +120,7 @@ function style(feature) {
     // fillColor: getColor(feature.properties.density)
   };
   console.log(cookie.id);
+  console.log(feature.properties.id);
   if(feature.properties.id == cookie.id){
     returnData.fillColor = 'red';
   }
