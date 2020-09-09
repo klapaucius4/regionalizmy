3a6af22dceaf57fc0d374a53189c23e0291e9d98
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-09 13:39:31 +0200

    mods

--- js/scripts.js
+++ js/scripts.js
@@ -96,7 +96,7 @@
 
   var map = L.map('rgm-map', {minZoom: 7, maxZoom: 10}).setView([51.759445, 19.457216], 6);
 
-  map.scrollWheelZoom.disable();
+  // map.scrollWheelZoom.disable();
 
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
