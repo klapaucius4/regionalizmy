2ebeb4f9c2a0ffe91acb0787099c2cd3244d4f31
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-09 19:48:25 +0200

    test

--- single.php
+++ single.php
@@ -43,7 +43,6 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded shadow-sm">
-            <img class="mr-3" src="../../assets/brand/bootstrap-outline.svg" alt="" width="48" height="48">
             <div class="lh-100">
               <h6 class="mb-0 text-white lh-100">Bootstrap</h6>
               <small>Since 2011</small>
