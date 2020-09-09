53cbe20d761624fef7cff8cea3ee01e82ca327dc
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-09 13:39:04 +0200

    mods

--- js/scripts.js
+++ js/scripts.js
@@ -96,6 +96,8 @@
 
   var map = L.map('rgm-map', {minZoom: 7, maxZoom: 10}).setView([51.759445, 19.457216], 6);
 
+  map.scrollWheelZoom.disable();
+
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -206,9 +208,6 @@
     });
   }
 
-
-
-  map.scrollWheelZoom.disable();
 
 
   map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
