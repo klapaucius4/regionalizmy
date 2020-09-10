868aa750b37ff386ac1b62546ff1c5a1ed5cd5ca
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-10 18:16:00 +0200

    mods

--- template-parts/loop-phrase.php
+++ template-parts/loop-phrase.php
@@ -7,7 +7,7 @@
     <p class="post-meta"><?= __('Dodane przez'); ?> <a href="#"><?= get_the_author(); ?></a> <?= get_the_date(); ?></p>
     </div>
     <div class="vote-buttons col-md-4 d-flex justify-content-end align-items-center">
-        <button class="btn btn-success mr-1" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="1"><?= __('Znam'); ?></button>
+        <button class="btn btn-success mr-1" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="1" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover"><?= __('Znam'); ?></button>
         <button class="btn btn-danger" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="0"><?= __('Nie znam'); ?></button>
     </div>
 </div>
