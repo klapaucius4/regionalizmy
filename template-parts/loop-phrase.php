82aaf717540b35e98373a1f37b61f93be7c2ad71
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-10 20:25:48 +0200

    mods

--- template-parts/loop-phrase.php
+++ template-parts/loop-phrase.php
@@ -10,7 +10,7 @@
         <div class="row">
             <div class="col-12 d-flex justify-content-end">
                 <button class="btn btn-success mr-1" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="1" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover"><?= __('Znam'); ?></button>
-                <button class="btn btn-danger" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="0"><?= __('Nie znam'); ?></button>
+                <button class="btn btn-danger" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="0" data-toggle="modal" data-target="#exampleModalCenter"><?= __('Nie znam'); ?></button>
             </div>
             <div class="col-12 text-right">
                 <div class="form-check form-check-inline">
@@ -20,4 +20,24 @@
             </div>
         </div>
     </div>
+</div>
+
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
 </div>
