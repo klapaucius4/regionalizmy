32532a84a242446f0ea436ce00d14a561e35bcd1
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-13 07:42:01 +0200

    mods

--- template-parts/loop-phrase.php
+++ template-parts/loop-phrase.php
@@ -7,18 +7,20 @@
     <p class="post-meta"><?= __('Dodane przez'); ?> <a href="#"><?= get_the_author(); ?></a> <?= get_the_date(); ?></p>
     </div>
     <div class="vote-buttons col-md-4 align-items-center">
-        <div class="row">
-            <div class="col-12 d-flex justify-content-end">
+        <!-- <div class="row"> -->
+            <div class="d-flex justify-content-end">
                 <button class="btn btn-success mr-1" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="1" data-toggle="popover" title="Popover Header" data-content="Some content inside the popover"><?= __('Znam'); ?></button>
                 <button class="btn btn-danger" data-phrase-id="<?= get_the_ID(); ?>" data-vote-value="0" data-toggle="modal" data-target="#exampleModalCenter"><?= __('Nie znam'); ?></button>
             </div>
+            <?php /*
             <div class="col-12 text-right">
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="1" checked>
-                    <?php /*<label class="form-check-label" for="inlineCheckbox1"><?= __('Słyszałem to w ') ?></label>*/ ?>
+                    <label class="form-check-label" for="inlineCheckbox1"><?= __('Słyszałem to w ') ?></label>
                 </div>
             </div>
-        </div>
+            */ ?>
+        <!-- </div> -->
     </div>
 </div>
 
