30d97587e1278ea7a3d87cba20eb191ad119b109
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-21 14:39:05 +0200

    mods

--- footer.php
+++ footer.php
@@ -48,55 +48,48 @@
       </div>
       <div class="modal-body">
         <form autocomplete="off" class="ui-widget">
-          <div class="vote-up">
-            <fieldset class="form-group fieldset1">
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="1" checked>
-                <?php
-                // $rgmUserCountyCookie = isset($_COOKIE['rgmUserCounty'])?json_decode(stripslashes($_COOKIE['rgmUserCounty']), true):null;
-                ?>
-                <label class="form-check-label" for="gridRadios1"></label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="2">
-                <label class="form-check-label" for="gridRadios2"><?= __('Znam z innego regionu Polski'); ?></label>
-              </div>
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="3">
-                <label class="form-check-label" for="gridRadios3"><?= __('Znam z ogólnopolskich środków masowego przekazu'); ?><small class="text-small d-block"><?= __('(prasa, radio, telewizja, Internet, książka, film)') ?></small></label>
-              </div>
-            </fieldset>
-            <fieldset class="form-group fieldset2">
-              <input type="text" class="form-control findCountyInput2 has-warning" placeholder="<?= __('Powiat / miasto powiatowe'); ?>">
+          <fieldset class="form-group fieldset1">
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="1" checked>
+              <?php
+              // $rgmUserCountyCookie = isset($_COOKIE['rgmUserCounty'])?json_decode(stripslashes($_COOKIE['rgmUserCounty']), true):null;
+              ?>
+              <label class="form-check-label" for="gridRadios1"></label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="2">
+              <label class="form-check-label" for="gridRadios2"><?= __('Znam z innego regionu Polski'); ?></label>
+            </div>
+            <div class="form-check">
+              <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios3" value="3">
+              <label class="form-check-label" for="gridRadios3"><?= __('Znam z ogólnopolskich środków masowego przekazu'); ?><small class="text-small d-block"><?= __('(prasa, radio, telewizja, Internet, książka, film)') ?></small></label>
+            </div>
+          </fieldset>
+          <fieldset class="form-group fieldset2">
+            <input type="text" class="form-control findCountyInput2 has-warning" placeholder="<?= __('Powiat / miasto powiatowe'); ?>">
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+              <label class="form-check-label" for="inlineCheckbox1"><?= __('ustaw jako domyślny region'); ?></label>
+            </div>
+          </fieldset>
+          <fieldset class="form-group fieldset3">
+          <?php
+          $args = array(
+            'post_type' => 'rgm_mass_media',
+            'posts_per_page' => -1,
+            'post_status' => 'publish'
+          );
+          $myQuery = new WP_Query($args);
+          if($myQuery->have_posts()):
+          ?>
+            <?php while($myQuery->have_posts()): $myQuery->the_post(); ?>
               <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                <label class="form-check-label" for="inlineCheckbox1"><?= __('ustaw jako domyślny region'); ?></label>
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio<?= get_the_ID(); ?>" value="option<?= get_the_ID(); ?>">
+                <label class="form-check-label" for="inlineRadio<?= get_the_ID(); ?>"><?= get_the_title(); ?></label>
               </div>
-            </fieldset>
-            <fieldset class="form-group fieldset3">
-              <?php for($i=1; $i<=10; $i++): ?>
-                <div class="form-check form-check-inline">
-                  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio<?= $i; ?>" value="option<?= $i; ?>">
-                  <label class="form-check-label" for="inlineRadio<?= $i; ?>"><?= $i; ?></label>
-                </div>
-              <?php endfor; ?>
-            </fieldset>
-          </div>
-          <div class="vote-down">
-            <fieldset class="form-group fieldset1">
-              <div class="form-check">
-                <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios2" value="2">
-                <label class="form-check-label" for="gridRadios2"><?= __('Znam z innego regionu Polski'); ?></label>
-              </div>
-            </fieldset>
-            <fieldset class="form-group fieldset2">
-              <input type="text" class="form-control findCountyInput2 has-warning" placeholder="<?= __('Powiat / miasto powiatowe'); ?>">
-              <div class="form-check form-check-inline">
-                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                <label class="form-check-label" for="inlineCheckbox1"><?= __('ustaw jako domyślny region'); ?></label>
-              </div>
-            </fieldset>
-          </div>
+            <?php endwhile; wp_reset_postdata(); ?>
+          <?php endif; ?>
+          </fieldset>
         </form>
       </div>
       <div class="modal-footer">
