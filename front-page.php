520b7f17fe8e41776ff59c12c20730ed2b3ffb89
klapaucius4 <klapaucius.mk@gmail.com>
2020-09-25 01:23:25 +0200

    mods

--- front-page.php
+++ front-page.php
@@ -72,7 +72,7 @@
       </div>
       <div class="row">
         <div class="col-md-12 text-center">
-          <a href="#" class="btn btn-primary"><?= __('Przejdź do słownika'); ?></a>
+          <a href="#" class="btn btn-secondary"><?= __('Przejdź do słownika'); ?></a>
         </div>
       </div>
     </div>
