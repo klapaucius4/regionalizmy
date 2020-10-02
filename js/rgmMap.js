0f02e9654736b4105d0a71baa2371250f0a661f4
klapaucius4 <klapaucius.mk@gmail.com>
2020-10-02 15:11:55 +0200

    mods

--- js/rgmMap.js
+++ js/rgmMap.js
@@ -20,11 +20,11 @@ info.onAdd = function (map) {
     return this._div;
 };
 
-// info.update = function (props) {
-//     this._div.innerHTML = '<h4>Lorem ipsum dolor</h4>' + (props ?
-//     '<b>' + props.name + '</b><br />' + '<span>' + props.subtitle + '</span>' + '<br /><br />' + props.density + ' people / mi<sup>2</sup>'
-//     : 'Hover over a state');
-// };
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Lorem ipsum dolor</h4>' + (props ?
+    '<b>' + props.name + '</b><br />' + '<span>' + props.subtitle + '</span>' + '<br /><br />' + props.density + ' people / mi<sup>2</sup>'
+    : 'Hover over a state');
+};
 
 info.addTo(map);
 
@@ -59,15 +59,15 @@ function resetHighlight(e) {
     info.update();
 }
 function setCurrentCounty(e) {
-    var newCookie = {
-        'id': e.target.feature.id,
-        'name': e.target.feature.properties.name
-    };
-    if($.cookie('rgmUserCounty', JSON.stringify(newCookie), { expires: 7 })){
-        geojson.resetStyle();
-        e.target.setStyle({ fillColor: 'red' });
-        $('.findCountyInput').val(e.target.feature.properties.name);
-    };
+    // var newCookie = {
+    //     'id': e.target.feature.id,
+    //     'name': e.target.feature.properties.name
+    // };
+    // if($.cookie('rgmUserCounty', JSON.stringify(newCookie), { expires: 7 })){
+    //     geojson.resetStyle();
+    //     e.target.setStyle({ fillColor: 'red' });
+    //     $('.findCountyInput').val(e.target.feature.properties.name);
+    // };
     
 }
 function highlightFeature(e) {
