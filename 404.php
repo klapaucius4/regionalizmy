d7ae756cf8161c496dfed8aae81743901301326c
klapaucius4 <klapaucius.mk@gmail.com>
2020-10-18 20:00:46 +0200

    dev mods next...

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<section class="section-space">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        404
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php
+get_footer();
