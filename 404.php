bcdc6adf10c2c128ecd364ade0acec507dfc7eb6
klapaucius4 <klapaucius.mk@gmail.com>
2020-11-03 23:11:55 +0100

    body background

--- 404.php
+++ 404.php
@@ -6,9 +6,9 @@
       <div class="col-md-7">
         <img src="<?= get_template_directory_uri(); ?>/img/404.svg" class="w-100" />
       </div>
-      <div class="col-md-5">
-        <h1>404</h1>
-        <p>Strona nie istnieje.</p>
+      <div class="col-md-5 text-center">
+        <h1>Strona nie istnieje</h1>
+        <!-- <p>Strona, której szukasz nie istneije.</p> -->
         <p><a href="<?= get_home_url(); ?>" class="btn btn-primary"><?= __('Powrót do strony głównej'); ?></a></p>
       </div>
     </div>
