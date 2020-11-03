85babd6cb84347318cafcaeaf0be842aece6404e
klapaucius4 <klapaucius.mk@gmail.com>
2020-11-03 23:25:17 +0100

    body background

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@
         <img src="<?= get_template_directory_uri(); ?>/img/404.svg" class="w-100" />
       </div>
       <div class="col-md-5 text-center">
-        <h1><?= __('404'); ?></h1>
+        <h1 class="color-primary"><?= __('404'); ?></h1>
         <h3><?= __('Nie odnaleziono strony'); ?></h3>
         <p><?= __('Strona, której szukasz nie istnieje. Upewnij się, że link do strony jest poprawny.'); ?></p>
         <p><a href="<?= get_home_url(); ?>" class="btn btn-primary"><?= __('Powrót do strony głównej'); ?></a></p>
