964c8a57d65a0608ddb1aea926ff0ae2c9514ac6
klapaucius4 <klapaucius.mk@gmail.com>
2020-12-16 08:37:45 +0100

    backend

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -6,8 +6,9 @@ register_post_type('rgm_phrase', array(
             'singular_name' => __('Fraza', 'regionalizmy'),
         ),
         'public' => true,
-        'has_archive' => __('slownik', 'regionalizmy'),
-        'rewrite' => array('slug' => __('slownik', 'regionalizmy'), 'with_front' => true),
+        'has_archive' => true,
+        // 'has_archive' => __('slownik', 'regionalizmy'),
+        // 'rewrite' => array('slug' => __('slownik', 'regionalizmy'), 'with_front' => true),
         'supports' => array( 'title' ),
         'menu_icon' => 'dashicons-editor-ol',
     )
