9f57821145cf82a5ad82969d41c17c1dba6affaa
klapaucius4 <klapaucius.mk@gmail.com>
2020-12-21 22:24:52 +0100

    archive of phrases

--- archive-rgm_phrase.php
+++ archive-rgm_phrase.php
@@ -1,5 +1,28 @@
 <?php get_header(); ?>
 
-test
+<section class="section-space">
+    <!-- Main Content -->
+    <div class="container">
+        <?php if($sectionSamplePhrases = get_field('sekcja_przykladowe_frazy')): ?>
+        <div class="row">
+            <div class="col-md-12 mb-5 text-center">
+            <h4>Lorem ipsum</h4>
+            <p class="m-0 small">dolor.....</p>
+            </div>
+        </div>
+        <div class="row">
+        <?php if(have_posts()): ?>
+            <div class="col-md-12">
+                <?php while(have_posts()): the_post(); ?>
+                    <?php get_template_part('template-parts/loop-phrase', ''); ?>
+                    <hr>
+                <?php endwhile; wp_reset_postdata(); ?>
+                <div class="clearfix"></div>
+            </div>
+        <?php endif; ?>
+        </div>
+        <?php endif; ?>
+    </div>
+</section>
 
 <?php get_footer();
