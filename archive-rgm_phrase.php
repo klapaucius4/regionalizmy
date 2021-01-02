1cfed6817088f6c32c5c33491e5dab8677bc254e
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-02 14:53:05 +0100

    modyfikacje z widokiem zbiorczym

--- archive-rgm_phrase.php
+++ archive-rgm_phrase.php
@@ -6,7 +6,9 @@
         <div class="row">
             <div class="col-md-12 mb-5 text-center">
             <h4><?= get_the_archive_title(); ?></h4>
-            <p class="m-0 small">dolor.....</p>
+            <?php if(is_category()): ?>
+            <p class="m-0 small"><?= get_the_archive_title(); ?></p>
+            <?php endif; ?>
             </div>
         </div>
         <div class="row">
