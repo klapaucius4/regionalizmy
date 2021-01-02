ba0ca7f3fc0879889bb96526fe44b8195c052522
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-02 15:00:14 +0100

    modyfikacje z widokiem zbiorczym

--- sidebar.php
+++ sidebar.php
@@ -14,36 +14,23 @@
 
     <!-- Categories Widget -->
     <div class="sidebar__card">
-        <h5 class="card-header sidebar__card__header">Categories</h5>
+        <h5 class="card-header sidebar__card__header"><?= __('Litery', 'regionalizmy'); ?></h5>
         <div class="card-body">
-        <div class="row">
-            <div class="col-lg-6">
-            <ul class="list-unstyled mb-0">
-                <li>
-                <a href="#">Web Design</a>
-                </li>
-                <li>
-                <a href="#">HTML</a>
-                </li>
-                <li>
-                <a href="#">Freebies</a>
-                </li>
-            </ul>
+        <?php
+        $letters = get_terms(array(
+            'taxonomy' => 'rgm_phrase_letter',
+            'hide_empty' => true,
+        ));
+        if($letters):
+        ?>
+            <div class="row">
+                <?php foreach($letters as $letter): ?>
+                <div class="col-md-2">
+                    <a href="<?= get_term_link($letter->taxonomy, $letter->term_id); ?>"><?= $letter->name; ?></a>
+                </div>
+                <?php endforeach; ?>
             </div>
-            <div class="col-lg-6">
-            <ul class="list-unstyled mb-0">
-                <li>
-                <a href="#">JavaScript</a>
-                </li>
-                <li>
-                <a href="#">CSS</a>
-                </li>
-                <li>
-                <a href="#">Tutorials</a>
-                </li>
-            </ul>
-            </div>
-        </div>
+        <?php endif; ?>
         </div>
     </div>
 
