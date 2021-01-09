51eb13fd02e829babd008a0fc0d26b8c28dd1069
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-09 20:21:07 +0100

    backend

--- inc/wordpress-settings.php
+++ inc/wordpress-settings.php
@@ -226,9 +226,8 @@ add_action( 'manage_rgm_phrase_posts_custom_column' , 'custom_rgm_phrase_column'
  
 function set_custom_edit_rgm_phrase_columns($columns) {
     unset( $columns['author'] );
-    $columns['rgm_phrase_meaning'] = __( 'Znaczenie' );
 
-    array_unshift($columns, $columns['rgm_phrase_meaning']);
+    $columns = array_merge(array('rgm_phrase_meaning'=>__( 'Znaczenie' )), $columns);
  
     return $columns;
 }
