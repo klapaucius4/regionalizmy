66361434eaac367bd16e4ac1d440f6b13bec409a
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-09 20:33:56 +0100

    backend

--- inc/wordpress-settings.php
+++ inc/wordpress-settings.php
@@ -229,7 +229,7 @@ function set_custom_edit_rgm_phrase_columns($columns) {
     // unset( $columns['date'] );
 
     // $columns['rgm_phrase_meaning'] = __('Znaczenie');
-    // var_dump($columns);
+    var_dump($columns); exit;
 
     $newColumnsOrder = array(
         'title' => $columns['taxonomy-rgm_phrase_kind'],
