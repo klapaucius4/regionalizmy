1d761e0547289d399d6102e8873b03724e6b0126
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-14 21:43:45 +0100

    making backend

--- inc/wordpress-settings.php
+++ inc/wordpress-settings.php
@@ -251,6 +251,7 @@ function custom_rgm_phrase_column( $column, $post_id ) {
                 echo $phraseDefinition;
             }elseif($meaning = get_field('znaczenie', $post_id)){
                 if(isset($meaning[0])){
+                    var_dump($meaning[0]);
                     echo '<a href="#">'.$meaning[0]->post_title.'</a>';
                     if($definition = get_field('znaczenie', $meaning[0]->ID)){
                         echo ' - ' . $definition;
