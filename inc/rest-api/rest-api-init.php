47e1f0e4dba26d53786629c1a506bec24074455b
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-23 13:44:20 +0100

    rest api

--- inc/rest-api/rest-api-init.php
+++ inc/rest-api/rest-api-init.php
@@ -1,8 +1,8 @@
 <?php
 require get_template_directory() . '/inc/rest-api/class-rgm-rest-controller.php';
 
-require get_template_directory() . '/inc/rest-api/class-rgm-rest-search-route.php';
-require get_template_directory() . '/inc/rest-api/class-rgm-rest-county-route.php';
+require get_template_directory() . '/inc/rest-api/class-rgm-rest-search-controller.php';
+require get_template_directory() . '/inc/rest-api/class-rgm-rest-county-controller.php';
 
 add_action( 'rest_api_init', function () {
     /**
