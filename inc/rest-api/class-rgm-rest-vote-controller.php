a495727be147cb3f3b67b5cd238e3f4c881867c8
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-24 09:05:11 +0100

    backend & rest api modifications

--- inc/rest-api/class-rgm-rest-vote-controller.php
+++ inc/rest-api/class-rgm-rest-vote-controller.php
@@ -9,38 +9,19 @@ class RGM_REST_Vote_Controller extends RGM_REST_Controller {
 
     public function register_routes() {
 
-        // register_rest_route( $this->namespace, '/' . $this->base, array(
-        //     array(
-        //       'methods'             => WP_REST_Server::CREATABLE,
-        //       'callback'            => array( $this, 'create_item' ),
-        //       'permission_callback' => array( $this, 'create_item_permissions_check' ),
-        //       'args'                => $this->get_endpoint_args_for_item_schema( true ),
-        //     ),
-        // ));
-
         register_rest_route( $this->namespace, '/' . $this->base, array(
-          array(
-            'methods'             => WP_REST_Server::READABLE,
-            'callback'            => array( $this, 'get_items' ),
-            'permission_callback' => array( $this, 'get_items_permissions_check' ),
-            'args'                => array(
-                's' => array(),
-                'type' => array(
-                  'default' => 'dictionary', // or 'blog' or 'all'
-                )
-              ),
-            )
+            array(
+              'methods'             => WP_REST_Server::CREATABLE,
+              'callback'            => array( $this, 'create_item' ),
+              'permission_callback' => array( $this, 'create_item_permissions_check' ),
+              'args'                => $this->get_endpoint_args_for_item_schema( true ),
+            ),
         ));
         
         register_rest_route( $namespace, '/' . $this->base . '/schema', array(
             'methods'  => WP_REST_Server::READABLE,
             'callback' => array( $this, 'get_public_item_schema' ),
         ));
-    }
-
-
-    public function get_items( $request ) {
-      var_dump('test 2222'); exit;
     }
 
 
