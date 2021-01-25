a1e2e9ca57f70c8d298a3ca3f21b669621b745ee
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-25 22:47:49 +0100

    backend modifications

--- template-parts/loop-phrase.php
+++ template-parts/loop-phrase.php
@@ -3,12 +3,11 @@
         <a href="<?= get_the_permalink(); ?>">
             <h2 class="post-title m-0"><?= get_the_title(); ?></h2>
             <!-- <h3 class="post-subtitle"><?= get_field('krotki_opis'); ?></h3> -->
-            <?php
-            $meaning = get_field('znaczenie');
-            if(isset($meaning[0])): ?>
-            <h3 class="post-subtitle"><?= $meaning[0]->post_title; ?></h3>
-            <?php endif; ?>
         </a>
+        <?php $meaning = get_field('znaczenie');
+            if(isset($meaning[0])): ?>
+            <p class="post-meta"><?= $meaning[0]->post_title; ?></p>
+        <?php endif; ?>
         <!-- <p class="post-meta"><?= __('Dodane przez', 'rgm'); ?> <a href="#"><?= get_the_author(); ?></a> <?= get_the_date(); ?></p> -->
     </div>
     <div class="vote-buttons col-md-4 d-flex justify-content-end align-items-center">
