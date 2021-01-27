14bb3bca76932395bc11c38db11e997bb83eccb6
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-27 09:10:54 +0100

    mods

--- header.php
+++ header.php
@@ -120,17 +120,25 @@
       </div>
       <div class="row">
         <div class="col-md-12">
+          <?php if($breadcrumbStructure = get_breadcrumb_structure()): ?>
           <!-- Breadcrumbs -->
           <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-              <?php
-                $frontpageID = get_option( 'page_on_front' );
-              ?>
+              <?php foreach($breadcrumbStructure as $crumb): ?>
+                <li class="breadcrumb__item breadcrumb-item">
+                  <?php if($crumb[1]): ?><a href="<?= $crumb[1]; ?>"><?php endif; ?>
+                    <?= $crumb[0]; ?>
+                  <?php if($crumb[1]): ?></a><?php endif; ?>
+                </li>
+              <?php endforeach; ?>
+              <?php /*
               <li class="breadcrumb__item breadcrumb-item"><a href="<?= get_the_permalink($frontpageID); ?>"><?= get_the_title($frontpageID); ?></a></li>
               <li class="breadcrumb__item breadcrumb-item"><a href="#">Library</a></li>
               <li class="breadcrumb__item breadcrumb-item breadcrumb__item--active active" aria-current="page">Data</li>
+              */ ?>
             </ol>
           </nav>
+          <?php endif; ?>
         </div>
       </div>
     </div>
