06383db57e99bfb08b7e1a9b90225da4fef35358
klapaucius4 <klapaucius.mk@gmail.com>
2021-01-27 08:19:55 +0100

    backend modifications

--- header.php
+++ header.php
@@ -115,7 +115,7 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h1><?= get_the_archive_title(); ?></h1>
+          <h1 class="mb-5"><?= get_the_archive_title(); ?></h1>
         </div>
       </div>
       <div class="row">
