956c8a002ece4bf1e9fc3069899b09d9f8621fc5
klapaucius4 <klapaucius.mk@gmail.com>
2021-02-10 23:24:09 +0100

    little frontend mods

--- single-rgm_phrase.php
+++ single-rgm_phrase.php
@@ -12,12 +12,6 @@
 
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded shadow-sm">
-            <div class="lh-100">
-              <h6 class="mb-0 text-white lh-100">Bootstrap</h6>
-              <small>Since 2011</small>
-            </div>
-          </div>
 
           <div class="my-3 p-3 bg-white rounded shadow-sm">
             <h6 class="border-bottom border-gray pb-2 mb-0">Recent updates</h6>
