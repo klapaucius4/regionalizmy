6a52b0fc2dc9623316aae5c22e0f8428816bbc50
klapaucius4 <klapaucius.mk@gmail.com>
2021-02-11 23:48:33 +0100

    backend & frontend dev

--- single-rgm_phrase.php
+++ single-rgm_phrase.php
@@ -10,30 +10,32 @@
         </div>
       </div>
 
+      <?php if($examples = get_field('przyklady_uzycia')): ?>
       <div class="row single-section__usage-examples">
         <div class="col-lg-8 col-md-10 mx-auto">
-
-          <?php if($examples = get_field('przyklady_uzycia')): ?>
-          <div class="my-3 p-3 bg-white rounded shadow-sm">
-            <h6 class="border-bottom border-gray pb-2 mb-0"><?= __('Przykłady użycia'); ?>:</h6>
-            <?php foreach($examples as $example): if(!$example['przyklad']) continue; ?>
-              <div class="media text-muted pt-3 d-flex">
-                <i class="fas fa-angle-double-right mr-4 mt-4"></i>
-                <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                  <div class="d-flex justify-content-between align-items-center w-100">
-                    <strong class="text-gray-dark"><?= nl2br($example['przyklad']); ?></strong>
+          <div class="row">
+            <div class="col-md-8">
+              <div class="my-3 p-3 bg-white rounded shadow-sm">
+                <h6 class="border-bottom border-gray pb-2 mb-0"><?= __('Przykłady użycia'); ?>:</h6>
+                <?php foreach($examples as $example): if(!$example['przyklad']) continue; ?>
+                  <div class="media text-muted pt-3 d-flex">
+                    <i class="fas fa-angle-double-right mr-4 mt-4"></i>
+                    <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                      <div class="d-flex justify-content-between align-items-center w-100">
+                        <strong class="text-gray-dark"><?= nl2br($example['przyklad']); ?></strong>
+                      </div>
+                      <span class="d-block">@username</span>
+                    </div>
                   </div>
-                  <span class="d-block">@username</span>
-                </div>
+                <?php endforeach; ?>
               </div>
-            <?php endforeach; ?>
+            </div>
+            <div class="col-md-4 single-section__usage-examples__bg-container"></div>
           </div>
-          <?php endif; ?>
         </div>
-        <!-- <div class="col-md-4 single-section__usage-examples__bg-container">
-            
-        </div> -->
+        
       </div>
+      <?php endif; ?>
 
     </div>
   </article>
