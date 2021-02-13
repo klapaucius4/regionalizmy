8afe4c42e162adfa88faae3198404ee5acf2437f
klapaucius4 <klapaucius.mk@gmail.com>
2021-02-14 00:06:28 +0100

    frontend

--- single-rgm_phrase.php
+++ single-rgm_phrase.php
@@ -11,30 +11,23 @@
               <div class="row">
                 <div class="col-md-12">
                   <table class="table table-bordered">
-                    <thead>
-                      <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                      </tr>
-                    </thead>
                     <tbody>
                       <tr>
-                        <th scope="row">1</th>
+                        <th scope="row">Regionalizm</th>
                         <td>Mark</td>
                         <td>Otto</td>
                         <td>@mdo</td>
                       </tr>
                       <tr>
-                        <th scope="row">2</th>
+                        <th scope="row">Znaczenie</th>
                         <td>Jacob</td>
                         <td>Thornton</td>
                         <td>@fat</td>
                       </tr>
                       <tr>
-                        <th scope="row">3</th>
-                        <td colspan="2">Larry the Bird</td>
+                        <th scope="row">Synonimy</th>
+                        <td>Larry the Bird</td>
+                        <td>Thornton</td>
                         <td>@twitter</td>
                       </tr>
                     </tbody>
