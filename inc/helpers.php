75e3f844505b872385606a384baa607c8da54523
klapaucius4 <klapaucius.mk@gmail.com>
2021-05-09 21:40:14 +0200

    frontend

--- inc/helpers.php
+++ inc/helpers.php
@@ -257,7 +257,7 @@ function get_breadcrumb_structure(){
 
   if(is_singular('rgm_phrase')){
     $breadrumbStructure[] = array(
-      __('Słownik'),
+      __('Słownik', 'rgm'),
       get_post_type_archive_link('rgm_phrase')
     );
     $breadrumbStructure[] = array(
@@ -265,7 +265,19 @@ function get_breadcrumb_structure(){
       false
     );
   }
+  elseif(is_post_type_archive('rgm_phrase')){
+    $breadrumbStructure[] = array(
+      __('Słownik', 'rgm'),
+      false
+    );
+  }
   elseif(is_page() || is_single()){
+    $breadrumbStructure[] = array(
+      get_the_title(),
+      false
+    );
+  }
+  else{
     $breadrumbStructure[] = array(
       get_the_title(),
       false
--- template-parts/header/title.php
+++ template-parts/header/title.php
@@ -5,9 +5,9 @@ if(is_singular('rgm_phrase')){
 }
 elseif(is_post_type_archive('rgm_phrase')){
     if(isset($_GET['litera'])){
-    echo '<span class="small">' . __('Frazy na literę:', 'rgm') . '</span>' . ' ' . '<i>' . strip_tags($_GET['litera']) . '</i>';
+        echo '<span class="small">' . __('Frazy na literę:', 'rgm') . '</span>' . ' ' . '<i>' . strip_tags($_GET['litera']) . '</i>';
     }else{
-    echo __('Słownik', 'rgm');
+        echo __('Słownik', 'rgm');
     }
 }
 elseif(get_queried_object()){
