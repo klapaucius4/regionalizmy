96b1953c73bfdba2611164fbae81e523860ab7a5
klapaucius4 <klapaucius.mk@gmail.com>
2021-05-09 21:15:05 +0200

    frontend

--- header.php
+++ header.php
@@ -117,17 +117,20 @@
         <div class="col-md-12">
           <h1 class="mb-5">
           <?php
-            if(is_single()){
+            if(is_singular('rgm_phrase')){
+              echo '<span class="small">'.__('Fraza') . ':</span> ' . get_the_title();
+            }
+            elseif(is_single() && is_page()){
               echo get_the_title();
             }
-            elseif(is_singular('rgm_phrase')){
-              echo '<span class="small">'.__('Fraza') . ':</span> ' . get_the_title();
+            elseif(is_post_type_archive('rgm_phrase')){
+              echo get_the_archive_title();
             }
             elseif(get_queried_object()){
               var_dump(get_queried_object()); exit;
             }
             else{
-              echo get_the_archive_title();
+              echo get_the_title();
             }
           ?>
           </h1>
