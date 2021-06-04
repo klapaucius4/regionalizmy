4cac034cab0ca04ead10345dcf3fe8759f449ba5
klapaucius4 <klapaucius.mk@gmail.com>
2021-06-04 23:21:37 +0200

    removed section space from page.php

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <?php while(have_posts()): the_post(); ?>
-  <section class="section-space">
+  <section class="">
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
