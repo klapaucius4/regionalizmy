2dfcc94eca46ba68943353f1a2ab94360507472d
klapaucius4 <klapaucius.mk@gmail.com>
2021-06-29 00:13:39 +0200

    search form at sidebar

--- sidebar.php
+++ sidebar.php
@@ -3,12 +3,14 @@
     <div class="sidebar__card">
         <h5 class="card-header sidebar__card__header"><?= __('Znajdź regionalizm', 'rgm'); ?></h5>
         <div class="card-body">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="<?= __('Znajdź regionalizm', 'rgm'); ?>">
-            <span class="input-group-append">
-            <button class="btn--smaller btn-secondary" type="button"><?= __('Szukaj', 'rgm'); ?></button>
-            </span>
-        </div>
+            <form action="<?= get_post_type_archive_link('rgm_phrase'); ?>" method="GET">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="<?= __('Znajdź regionalizm', 'rgm'); ?>">
+                    <span class="input-group-append">
+                    <button class="btn--smaller btn-secondary" type="button"><?= __('Szukaj', 'rgm'); ?></button>
+                    </span>
+                </div>
+            </form>
         </div>
     </div>
 
@@ -18,9 +20,9 @@
         <div class="card-body">
         <?php
         $letters = alphabeticalListOfLetters();
-        if($letters): ?>
+        if ($letters) : ?>
             <div class="row">
-                <?php foreach($letters as $letter): ?>
+                <?php foreach ($letters as $letter) : ?>
                 <div class="col col-2">
                     <a href="<?= get_post_type_archive_link('rgm_phrase'); ?>?<?= http_build_query(array('litera' => $letter), null, "&", PHP_QUERY_RFC3986); ?>" class="<?= (isset($_GET['litera']) && $_GET['litera'] == $letter)?'active':''; ?>"><?= mb_strtoupper($letter, "UTF-8"); ?></a>
                 </div>
@@ -36,9 +38,9 @@
         <div class="card-body">
             <?php
             $args = array(
-                'taxonomy' => array( 'rgm_phrase_tag' ), 
+                'taxonomy' => array( 'rgm_phrase_tag' ),
             );
-            wp_tag_cloud( $args );
+            wp_tag_cloud($args);
             ?>
         </div>
     </div>
